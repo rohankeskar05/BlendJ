3352e93ba7e6b86937dd363c809e274deb49c202
rohankeskar05 <35923489+rohankeskar05@users.noreply.github.com>
2018-12-23 13:37:03 +0530

    First

---
+++ First.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author comp
+ *
+ */
+public class FirstPrgm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stu
+		System.out.println("Hi, I am learning following subjects:");
+		System.out.println("1. Devops Basics");
+		System.out.println("2. GitHub");
+		System.out.println("3. Version controlling");
+		System.out.println("3. Version controlling");
+
+	}
+
+}
